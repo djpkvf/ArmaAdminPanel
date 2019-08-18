5fc47a872123981483602ce5b5ea69823271caf0
Dominic Pilla <dominic.pilla@keefegroup.com>
2019-08-18 14:30:15 -0500

    Initial Commit

---
+++ ArmaAdminPanel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArmaAdminPanel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
